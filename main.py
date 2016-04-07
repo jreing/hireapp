06ba331dc782a2b54d273d6a9e0c10dd9638fd37
jreing <fanpeterssohn@gmail.com>
2016-04-07 13:18:56 +0300

    started company page db support

--- main.py
+++ main.py
@@ -13,37 +13,75 @@ class Course(ndb.Model):
 	course_name = ndb.StringProperty(indexed=False, required=True)
 	course_type = ndb.StringProperty(indexed=False, required=True)
 	
-class Student_Course(ndb.Model):
-	grade = ndb.IntegerProperty(indexed=False, required=True)
-	weight = ndb.IntegerProperty(indexed=False, required=True)
-	semester = ndb.StringProperty(indexed=False, required=True)
-	course= ndb.StructuredProperty (Course, required=True)
-	student_id= ndb.IntegerProperty	(indexed=True, required=True)
-	hashed_id = ndb.IntegerProperty(indexed=False)
-
-class dbBuild(webapp2.RequestHandler):
-	inputfile=open("courses.csv")
-	text = inputfile.readlines()
-	for i in range(0,len(text)):
-		c=Course(course_id=text[i], course_name=text[i+1], course_type="class")
-		c.put()
-		i+=1
-	inputfile.close()
-	self.response.write('Database built')
+class Student(ndb.Model):
+	id = ndb.StringProperty(indexed=True, required=True)
+	name= ndb.StringProperty(indexed=True, required=True)
+	city =ndb.StringProperty(indexed=True, required=True)
 	
+class Student_Course(ndb.Model):
+	grade = ndb.IntegerProperty(indexed=True, required=True)
+	#weight = ndb.IntegerProperty(indexed=False, required=True)
+	#semester = ndb.StringProperty(indexed=False, required=True)
+	course= ndb.StructuredProperty (Course, required=True)
+	student= ndb.StructuredProperty (Student, required=True)
+	#hashed_id = ndb.IntegerProperty(indexed=False)
+
+class minGradeQuery(webapp2.RequestHandler):
+	def post(self):	 
+		course_name=self.request.get('name')
+		grade= int(self.request.get('grade'))
+		self.response.write('<html><body>')
+		self.response.write(grade)
+		#Student_Course.grade>=grade
+		query=Student_Course.query()
+		self.response.write(query)
+		## TODO: write the response in a nicer way
+		query.fetch(100)
+		for student in query:
+			self.response.write("Student %s\n" %student)
+		self.response.write('End of Results</html></body>')
+		
+
+
+	
+class dbBuild(webapp2.RequestHandler):
+	def get(self):
+		inputfile=open("courses.csv")
+		text = inputfile.readlines()
+		for i in range(0,len(text)-2,2):
+			c=Course(course_name=text[i],course_id=text[i+1], course_type="class")
+			c.put()
+			
+		inputfile.close()
+		self.response.write('Database built')
+	
+class dbDelete(webapp2.RequestHandler):
+	def get(self):
+		ndb.delete_multi(
+			Course.query().fetch(keys_only=True)
+		)
+		self.response.write('Database deleted')
+	
+		
 		
 class dbHandler(webapp2.RequestHandler):
     def post(self):	 
-    
-	
-	course_name=self.request.get('name')
-	c=Course(course_id='1', course_name=course_name, course_type="class")
-	
-	c.put()
-	self.response.write('<html><body>')
-	self.response.write('Test Entry added')
+		course_name=self.request.get('name')
+		grade= int(self.request.get('grade'))
+		c=Course(course_id='1', course_name=course_name, course_type="class")
+		s=Student_Course(grade=grade, course=c) 
+		s.put()
+		## TODO: write the response in a nicer way
+		self.response.write('<html><body>Test Entry added</body></html>')
 
 
+class CompanyHandler(webapp2.RequestHandler):
+    def get(self):
+        f = open("companyQueryFormPage\index.html") 
+	#self.response.charset="unicode"
+	self.response.write(f.read())
+	f.close()        
+		
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         f = open("studentinputpage\index.html") 
@@ -53,6 +91,11 @@ class MainHandler(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/dbHandler', dbHandler)
-	('/dbBuild', dbHandler)
+    ('/dbHandler', dbHandler),
+	('/dbDelete', dbDelete),
+	('/dbBuild', dbBuild),
+	('/studentinputpage/index.html', MainHandler),
+	('/companyQueryFormPage/index.html', CompanyHandler),
+	('/companyQueryFormPage/minGradeQuery' , minGradeQuery)
+	
 ], debug=True)
