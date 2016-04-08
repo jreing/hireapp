2aaf2678df3eabf0d9fb6d36c187785c6b8f32fb
codeknitting <drorhag@gmail.com>
2016-04-08 17:59:10 +0300

    last update

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ class minGradeQuery(webapp2.RequestHandler):
 		self.response.write(grade )
 		self.response.write("<br>End of Debug prints<br><br>")
 		q=Student_Course.query(Student_Course.grade>=grade, Student_Course.course.course_name==course_name)
-		self.response.write(q)
+		#self.response.write(q)
 		## TODO: write the response in a nicer way
 		q.fetch(100)
 		for student in q:
