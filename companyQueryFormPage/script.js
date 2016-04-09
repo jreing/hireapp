e0f7f5493b7560221f58557ecb49c1c97253d81c
barak50 <barakmizrahi@mail.tau.ac.il>
2016-04-09 16:42:44 +0300

    web pages fixed and improved

--- companyQueryFormPage/script.js
+++ companyQueryFormPage/script.js
@@ -2,8 +2,41 @@ var newNum = 0;
 var elems = 1;
 var newNumtwo = 0;
 var elemstwo = 1;
-alert("Hello! I am an alert box!!");
-$('#buttonadd').click(function() {
+
+function b() {
+  var num = this.id.substring(9);
+  elems--;
+
+  $('#cloneme' + num).remove(); // remove the correct element
+
+  //if only one element remains, disable the "remove" button
+  if (elems == 1) {
+	    var inputs = document.getElementsByClassName("buttondel");
+	for (var i = 0; i < inputs.length; i++) {
+        inputs[i].disabled = true;
+	}
+  }
+}
+
+function btwo() {
+  var numtwo = this.id.substring(12);
+  elemstwo--;
+
+  $('#clonemetwo' + numtwo).remove(); // remove the correct element
+
+  //if only one element remains, disable the "remove" button
+  if (elemstwo == 1) {
+	    var inputs = document.getElementsByClassName("buttondeltwo");
+	for (var i = 0; i < inputs.length; i++) {
+        inputs[i].disabled = true;
+	}
+  }
+}
+
+
+
+
+$('#buttonadd').click(function ab() {
   elems++;
   newNum = new Number(newNum + 1); // the numeric ID of the new input field being added
   // create the new element via clone(), and manipulate it's ID using newNum value
@@ -13,56 +46,51 @@ $('#buttonadd').click(function() {
   newElem.children().eq(0).val('');
   newElem.children().eq(1).val('');
   newElem.children().eq(2).attr('id', 'buttondel' + newNum).attr('class', 'buttondel');
-
+ 
+  newElem.children().eq(2)[ 0 ].onclick= b;
+ 
   // insert the new element after the last "duplicatable" input field
   //$('#cloneme' + (newNum-1)).after(newElem);
   $(newElem).insertBefore('#bysubject');
   // enable the "remove" button
-  $('.buttondel').attr('disabled', '');
+  //$('.buttondel').attr('disabled', '');
+  var inputs = document.getElementsByClassName("buttondel");
+	for (var i = 0; i < inputs.length; i++) {
+        inputs[i].disabled = false;
+	}
 
 
 });
 
-$('.buttondel').click(function() {
-  var num = this.id.substring(9);
-  elems--;
-
-  $('#cloneme' + num).remove(); // remove the last element
-
-  //if only one element remains, disable the "remove" button
-  if (elems == 1)
-    $('.buttondel').attr('disabled', 'disabled');
-});
-
-$('#buttonaddtwo').click(function() {
+$('#buttonaddtwo').click(function abtwo() {
   elemstwo++;
   newNumtwo = new Number(newNumtwo + 1); // the numeric ID of the new input field being added
   // create the new element via clone(), and manipulate it's ID using newNum value
   //var newElem = $('#cloneme' + (newNum - 1)).clone().attr('id', 'cloneme' + newNum);
-  var newElem = $('.clonemetwo:first').clone().attr('id', 'clonemetwo' + newNumtwo).attr('class', 'clonemetwo');
+  var newElemtwo = $('.clonemetwo:first').clone().attr('id', 'clonemetwo' + newNumtwo).attr('class', 'clonemetwo');
   // manipulate the name/id values of the input inside the new element
-  newElem.children().eq(0).val('');
-  newElem.children().eq(1).val('');
-  newElem.children().eq(2).attr('id', 'buttondeltwo' + newNumtwo).attr('class', 'buttondeltwo');
-
+  newElemtwo.children().eq(0).val('');
+  newElemtwo.children().eq(1).val('');
+  newElemtwo.children().eq(2).attr('id', 'buttondeltwo' + newNumtwo).attr('class', 'buttondeltwo');
+ 
+  newElemtwo.children().eq(2)[ 0 ].onclick= btwo;
+ 
   // insert the new element after the last "duplicatable" input field
   //$('#cloneme' + (newNum-1)).after(newElem);
-  $(newElem).insertBefore('#avgentry');
+  $(newElemtwo).insertBefore('#avgentry');
   // enable the "remove" button
-  $('.buttondeltwo').attr('disabled', '');
+  //$('.buttondel').attr('disabled', '');
+  var inputs = document.getElementsByClassName("buttondeltwo");
+	for (var i = 0; i < inputs.length; i++) {
+        inputs[i].disabled = false;
+	}
+
 
 });
 
-$('.buttondeltwo').click(function() {
-  var num = this.id.substring(12);
-  elemstwo--;
 
-  $('#clonemetwo' + num).remove(); // remove the last element
 
-  //if only one element remains, disable the "remove" button
-  if (elemstwo == 1)
-    $('.buttondeltwo').attr('disabled', 'disabled');
-});
-
-$('.buttondeltwo').attr('disabled', 'disabled');
+$('.buttondel')[ 0 ].onclick= b;
 $('.buttondel').attr('disabled', 'disabled');
+$('.buttondeltwo')[ 0 ].onclick= btwo;
+$('.buttondeltwo').attr('disabled', 'disabled');
