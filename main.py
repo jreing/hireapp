ca0e28474cfa38a19ab10d7fd9fb9690ab81f711
jreing <fanpeterssohn@gmail.com>
2016-04-11 19:54:23 +0300

    new update

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class MainPage(webapp2.RequestHandler):
 			self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
 			self.response.write('Hello, ' + user.nickname())
 			# self.response.write('<div><a href="/chooseEmployOrStudentPage/index.html">login</a></div>')				
-			self.response.write('<html> <script src="https://apis.google.com/js/platform.js" async defer></script>')
+			self.response.write('<html> <script src="https://apis.google.com/js/platform.js?onload=onLoadCallback" async defer></script>')
 			self.response.write('<meta name="google-signin-client_id" content="412529039560-acrgsqrqqit5no5d8am0jajjtei5jqua.apps.googleusercontent.com">')
 			self.response.write('<div class="g-signin2" data-onsuccess="onSignIn"></div>')
 			self.response.write("""<script> function onSignIn(googleUser){
