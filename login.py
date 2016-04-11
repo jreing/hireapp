31287c5184b8b912d3d565fe2c2076e81d77f905
jreing <fanpeterssohn@gmail.com>
2016-04-11 14:08:30 +0300

    this is a commit

---
+++ login.py
@@ -0,0 +1,11 @@
+#from oauth2client.client import flow_from_clientsecrets
+from google.appengine.api import oauth
+import logging
+import traceback
+
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        f = open("chooseEmployOrStudentPage/index.html") 
+	#self.response.charset="unicode"
+	self.response.write(f.read())
+	f.close() 
