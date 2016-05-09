214b1c76ff2c01167a355ac234baf5addcbae17d
jreing <fanpeterssohn@gmail.com>
2016-05-09 10:48:35 +0300

    fixed logout FINALLY!!!!

--- main.py
+++ main.py
@@ -134,29 +134,6 @@ class StudentEditHandler(webapp2.RequestHandler):
 		page = buildStudentEditPage(student_query)
 		self.response.write(page)
 
-class Logout(webapp2.RequestHandler):
-	def get(self):
-		user_id = self.request.cookies.get('id')
-		self.response.delete_cookie("id")
-		self.response.write("Logged out")
-		import Cookie
-		# On the production instance, we just remove the session cookie, because
-		# redirecting users.create_logout_url(...) would log out of all Google
-		# (e.g. Gmail, Google Calendar).
-		#
-		# It seems that AppEngine is setting the ACSID cookie for http:// ,
-		# and the SACSID cookie for https:// . We just unset both below.
-		cookie = Cookie.SimpleCookie()
-		cookie['ACSID'] = ''
-		cookie['ACSID']['expires'] = -86400  # In the past, a day ago.
-		self.response.headers.add_header(*cookie.output().split(': ', 1))
-		cookie = Cookie.SimpleCookie()
-		cookie['SACSID'] = ''
-		cookie['SACSID']['expires'] = -86400
-		self.response.headers.add_header(*cookie.output().split(': ', 1))
-		self.redirect("/") 
-
-
 
 
 app = webapp2.WSGIApplication([
@@ -167,7 +144,6 @@ app = webapp2.WSGIApplication([
 	('/StudentWelcomePage/index.html', WelcomeHandler),	
 	('/studenthandler', StudentHandler),
 	('/tokenSignIn', tokenSignIn),
-	('/StudentLogout', Logout),
 	('/dbHandler', dbHandler),
 	('/companyQueryFormPage', CompanyHandler),
 	('/companyQueryResultsPage' , minGradeQuery),
