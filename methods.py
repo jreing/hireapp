cc721d5c9a82463731a8818852386bbd41c91dfc
barak50 <barakmizrahi@mail.tau.ac.il>
2016-05-22 21:28:45 +0300

    attempt at fixing autocomplete bug

--- methods.py
+++ methods.py
@@ -211,7 +211,7 @@ def buildStudentInputPage(course_query):
 		      <input type="button" id="buttonadd" value="הוסף קורס" />
 		    </div>
 		    <div id="cloneme0" class="cloneme">
-		      <input name="name" type="text" list="courses" class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="שם קורס" id="name" autocomplete="off"/>
+		      <input name="name" type="text" list="courses" class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="שם קורס" id="name" />
 		      <input name="grade" type="number" class="validate[required,custom[email]] feedback-input2" min="60" max="100" id="grade" placeholder="ציון" />
 		      <input type="button" id="buttondel0" class="buttondel" value="X" />
 			  
@@ -291,7 +291,7 @@ def buildCompanyQuery(course_query):
 			  <input type="button" id="buttonadd" value="הוסף קורס" />
 			</div>
 			<div id="cloneme0" class="cloneme">
-			  <input name="name" type="text" list="courses" class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="שם קורס" id="name" autocomplete="off" />
+			  <input name="name" type="text" list="courses" class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="שם קורס" id="name"  />
 			  <input name="grade" type="number" class="validate[required,custom[email]] feedback-input2" min="60" max="100" id="grade" placeholder="ציון" />
 			  <input type="button" id="buttondel0" class="buttondel" value="X" />
 			</div>
