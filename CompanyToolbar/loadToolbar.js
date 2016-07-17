40bfee8b6439cb0ac2eaf6cde6362f151e352aae
barak50 <barakmizrahi@mail.tau.ac.il>
2016-07-17 20:10:14 +0300

    Update loadToolbar.js

--- CompanyToolbar/loadToolbar.js
+++ CompanyToolbar/loadToolbar.js
@@ -1,5 +1,5 @@
 document.body.innerHTML = '<div id="includedContent"></div>'+document.body.innerHTML;
-
+//adds toolbar to page that includes this javascript
 $(function loadtoolbar(){
 	$.post("/validateCompany", function(data){	
 		if (data.split("#")[1]=="accepted"){
