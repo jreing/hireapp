653df385a5a252253929eb9877f9f95c737c31e0
barak50 <barakmizrahi@mail.tau.ac.il>
2016-07-17 18:21:48 +0300

    documented toolbar

--- toolbar/loadtoolbar.js
+++ toolbar/loadtoolbar.js
@@ -1,4 +1,5 @@
 document.body.innerHTML = '<div id="includedContent"></div>'+document.body.innerHTML;
+//script to load toolbar to the html page that includes this script
 $(function loadtoolbar(){
       $("#includedContent").load("/toolbar/index.html");
 	  
