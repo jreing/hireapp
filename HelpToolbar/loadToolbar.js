1fab10cfa607ed517a129cb26f3c1c6acee5d833
barak50 <barakmizrahi@mail.tau.ac.il>
2016-07-17 19:58:24 +0300

    Update loadToolbar.js

--- HelpToolbar/loadToolbar.js
+++ HelpToolbar/loadToolbar.js
@@ -1,4 +1,5 @@
 document.body.innerHTML = '<div id="includedContent"></div>'+document.body.innerHTML;
+//function to load toolbar to html page that includes the script
 $(function loadtoolbar(){
 	$("#includedContent").load("/HelpToolbar/index.html");
 });
